7bdb706d596c13a95bdd933f9bc7b5dbddd3414a
Anna Zharkova <annagzh@gmail.com>
2017-07-12 16:55:47 -0700

    uncomment auth

--- server/app.js
+++ server/app.js
@@ -13,14 +13,14 @@ app.use(middleware.bodyParser.json());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// app.use(middleware.auth.session);
-// app.use(middleware.passport.initialize());
-// app.use(middleware.passport.session());
-// app.use(middleware.flash());
+app.use(middleware.auth.session);
+app.use(middleware.passport.initialize());
+app.use(middleware.passport.session());
+app.use(middleware.flash());
 
 app.use(express.static(path.join(__dirname, '../public/dist/')));
 
-app.use('/', routes.api);
+app.use('/', routes.auth);
 app.use('/api', routes.api);
 app.use('/api/profiles', routes.profiles);
 
