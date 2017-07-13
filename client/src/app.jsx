94e36398a68ceed02fc5272110c73a9dd31b7939
Anna Zharkova <annagzh@gmail.com>
2017-07-12 20:14:34 -0700

    update components & init upload button

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,19 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import $ from 'jquery';
+import $ from 'jquery';
 import FriendList from './components/FriendList.jsx';
+var ReactS3Uploader = require('react-s3-uploader');
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentVideo: ''
+      currentVideo: null
     }
+    this.onUpload = this.onUpload.bind(this);
   }
 
+  onUpload(e) {
+    // console.log('onUpload!');
+    // var reader = new FileReader();
+    // var file = e.target.files[0];
+    // reader.onloadend = () => {
+    //   this.setState({
+    //     currentVideo: file
+    //   });
+    // }
+    ReactS3Uploader.onUpload
+  }
+  // style={{marginRight: spacing + 'em'}}
   render () {
     return (<div>
       <h1>VR Stories</h1>
+      <form>
+      <input id="myInput" type="file" style={{visibility:'hidden'}} onChange={this.onUpload}/>
+      <input type="button" value="Show Dialog" ref="myInput" />
+      </form>
       <FriendList />
     </div>)
   }
--- client/src/components/FriendList.jsx
+++ client/src/components/FriendList.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import $ from 'jquery';
+import FriendItem from './FriendItem.jsx';
+
+var mockFriends = [
+  { name: 'Alex', videos: [null] },
+  { name: 'Corey', videos: [null] },
+  { name: 'David', videos: [null] }
+];
 
 class FriendList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      friends: mockFriends
+    };
   }
 
   render () {
-    return (<div>
-      <p>FriendList Component</p>
-    </div>)
+    return (
+      <div>
+        {this.state.friends.map((friend) => {
+          return ( <FriendItem friend={friend}/> )
+        })}
+     </div>)
   }
 }
 
