c84692bf9c4c11d13ab1b9d6f7543a7a1f59bbbc
Alexander Schnapp <alexanderschnapp@gmail.com>
2017-07-15 09:04:23 -0700

    Add s3 config data

--- server/controllers/upload.js
+++ server/controllers/upload.js
@@ -1,21 +1,20 @@
 const AWS = require('aws-sdk');
+const AWSconfig = require('config')['AWS'];
 
 AWS.config.update({
-  accessKeyId: '', //need location
-  secretAccessKey: '', //need exact location
+  accessKeyId: AWSconfig.s3.accessKeyId,
+  secretAccessKey: AWSconfig.s3.secretAccessKey
 });
 
 let s3 = new AWS.S3();
 
 module.exports.save = (req, res) => {
-  console.log(req.files);
-  console.log(Date.now().toString());
   let params = { Bucket: 'vrstories', Key: Date.now().toString(), Body: req.files[0].buffer, ContentType: req.files[0].mimetype};
   s3.putObject(params, (err, data) => {
     if (err) {
       console.log(err);
     } else {
-      console.log('Successfully uploaded data to vrstories');
+      console.log('Successfully uploaded data to vrstories bucket');
       res.end();
     }
   });
