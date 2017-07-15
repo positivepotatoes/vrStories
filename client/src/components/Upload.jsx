7ff97dee07ae36a571ce26fef1bb338f0ef627c4
Anna Zharkova <annagzh@gmail.com>
2017-07-14 19:13:21 -0700

    some progress on s3

--- client/src/components/Upload.jsx
+++ client/src/components/Upload.jsx
@@ -2,20 +2,46 @@ import React from 'react';
 import Dropzone from 'react-dropzone';
 import axios from 'axios';
 
-const Upload = (props) => {
-  let onDrop = (acceptedFiles, rejectedFiles) => {
-    console.log(acceptedFiles);
-    let formData = new FormData();
-    formData.append('file', acceptedFiles[0]);
-    axios.post('/api/upload', formData);
-  };
-  return (
-    <div className="dropzone">
-      <Dropzone onDrop={onDrop.bind(this)}>
-        <p>Drop a file here</p>
-      </Dropzone>
-    </div>
-  );
-};
+class Upload extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  onDrop(acceptedFiles, rejectedFiles) {
+    console.log('acceptedFiles:', acceptedFiles);
+    // let formData = new FormData();
+    // formData.append('file', acceptedFiles[0]);
+    // axios.post('/api/upload', formData);
+
+    // var buffer = Buffer(acceptedFiles[0]);
+    // console.log('buffer:', buffer);
+  }
+
+  render() {
+    return (
+      <div className="dropzone">
+        <Dropzone onDrop={this.onDrop.bind(this)}>
+          <p>Drop a file here</p>
+        </Dropzone>
+      </div>
+    );
+  }
+}
+
+// const Upload = (props) => {
+//   let onDrop = (acceptedFiles, rejectedFiles) => {
+//     console.log(acceptedFiles);
+//     let formData = new FormData();
+//     formData.append('file', acceptedFiles[0]);
+//     axios.post('/api/upload', formData);
+//   };
+//   return (
+//     <div className="dropzone">
+//       <Dropzone onDrop={onDrop.bind(this)}>
+//         <p>Drop a file here</p>
+//       </Dropzone>
+//     </div>
+//   );
+// };
 
 export default Upload;
--- server/controllers/upload.js
+++ server/controllers/upload.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const aws = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
@@ -18,3 +19,19 @@ let upload = multer({
 module.exports.save = (req, res) => {
   console.log(req.body);
 };
+=======
+// Load the SDK and UUID
+var AWS = require('aws-sdk');
+
+// Create an S3 client
+var s3 = new AWS.S3();
+
+var params = {Bucket: 'vr-stories', Key: Date.now().toString(), Body: 'Hello World!'};
+s3.putObject(params, function(err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Successfully uploaded data to vr-stories');
+  }
+});
+>>>>>>> some progress on s3
