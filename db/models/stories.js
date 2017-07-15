6ad2cb6b3877cf9c448e5f7d26f1860ad6f1d719
David Oh <davidjohis@gmail.com>
2017-07-15 16:51:24 -0700

    added related data in models

--- db/models/stories.js
+++ db/models/stories.js
@@ -1,7 +1,11 @@
 const db = require('../');
+const User = require('./profiles');
 
 const Story = db.Model.extend({
-  tableName: 'stories'
+  tableName: 'stories',
+  user: function() {
+    return this.belongsTo(Profile, 'profile_id');
+  }
 });
 
 module.exports = db.model('Story', Story);
