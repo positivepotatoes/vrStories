4c81b523031bddddd1b2d75cfd3b115617abd123
Alexander Schnapp <alexanderschnapp@gmail.com>
2017-07-17 02:33:44 -0700

    Next photo after 5 seconds

--- client/src/components/MediaFrame.jsx
+++ client/src/components/MediaFrame.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 
-const MediaFrame = ({currentMedia, friends, currentFriend}) => {
+const MediaFrame = ({setMediaState, currentMedia, friends, currentFriend}) => {
   let media;
+  let mediaTotal = currentFriend.videos.length;
+  let allPlayed = false;
+  currentMedia < mediaTotal - 1 ? allPlayed : allPlayed = true;
   if (currentFriend.videos[currentMedia].type.slice(0, 5) === 'video') {
+    let onEnd = () => {
+      console.log('ended');
+      setMediaState(allPlayed);
+    };
     media = (
       <video width="800" autoPlay>
-        <source src={currentFriend.videos[currentMedia].link} type={currentFriend.videos[currentMedia].type}></source>
+        <source src={currentFriend.videos[currentMedia].link} type={currentFriend.videos[currentMedia].type} onEnded={onEnd}></source>
       </video>
     );
   } else {
     media = (<img width="800" src={currentFriend.videos[currentMedia].link}></img>);
+    let timeout = setTimeout(() => {
+      setMediaState(allPlayed);
+      clearTimeout(timeout);
+    }, 5000);
   }
   return (
     <div>
