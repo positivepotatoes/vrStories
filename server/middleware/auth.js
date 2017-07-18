24575b910a7db0c0e327d23761f193e38aebb44a
Alexander Schnapp <alexanderschnapp@gmail.com>
2017-07-17 19:14:41 -0700

    Redis credentials

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,24 +1,42 @@
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
-const redisClient = require('redis').createClient();
-const config = require('config');
+const url = require('url');
 
 module.exports.verify = (req, res, next) => {
   if (req.isAuthenticated()) {
     return next();
   }
   res.redirect('/login');
-  next();
 };
 
-module.exports.session = session({
-  store: new RedisStore({
-    client: redisClient,
-    url: config.redis.url,
-    host: config.redis.host,
-    port: config.redis.port
-  }),
-  secret: 'more laughter, more love, more life',
-  resave: false,
-  saveUninitialized: false
-});
+let redisSession;
+let redisClient;
+
+if (process.env.REDIS_URL) {
+  const params = url.parse(process.env.REDIS_URL);
+  const auth = params.auth.split(':');
+  redisClient = require('redis').createClient(params.port, params.hostname);
+  redisClient.auth(params.auth.split(':')[1]);
+  redisSession = session({
+    store: new RedisStore({
+      client: redisClient
+    }),
+    secret: 'more laughter, more love, more life',
+    resave: false,
+    saveUninitialized: false
+  });
+} else {
+  redisClient = require('redis').createClient();
+  redisSession = session({
+    store: new RedisStore({
+      client: redisClient,
+      host: 'localhost',
+      port: 6379
+    }),
+    secret: 'more laughter, more love, more life',
+    resave: false,
+    saveUninitialized: false
+  });
+}
+
+module.exports.session = redisSession;
