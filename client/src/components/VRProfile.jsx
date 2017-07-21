157926929c74a7d94720f0af58c72cde45dc6776
Corey <cry@Coreys-MacBook-Air.local>
2017-07-21 16:13:31 -0700

    Working friend profiles ring

--- client/src/components/VRProfile.jsx
+++ client/src/components/VRProfile.jsx
@@ -10,23 +10,24 @@ const VRProfile = props => {
   let playing = '';
   let spacing = 0.2;
 
+
+
   if (props.currentStory.id === props.friend.profile.id) {
     playing = ` I AM PLAYING!!! ${props.currentStory.index}`;
     picRadius = 1.2;
   }
 
   return (
-    <Entity>
+    <Entity position={{x: props.x, y: props.y, z: props.z}} rotation={{x: props.xRotation, y: props.yRotation, z: 0}}>
       <Entity id='cylinder'
         geometry={{primitive: 'cylinder', radius: picRadius, height: 0.15}}
         rotation= "0 90 90"
         material={{src: props.friend.profile.img_url}}
         animation__rotate={{property: 'rotation', dur: 2000, loop: true, to: '360 360 360'}}
         animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '1.1 1.1 1.1'}}
-        position={{x: props.x + spacing, y: 0, z: -3}}
         events={{click: (() => props.onFriendClick(props.friend))}}>
       </Entity>
-      <Entity text={{value: props.friend.profile.first + playing, align: 'center', color: 'white', width: 3}} position={{x: props.x, y: -1.3, z: -3}}/>
+      <Entity text={{value: props.friend.profile.first + playing, align: 'center', color: 'white', width: 6}} position={{y: -1.8}}/>
     </Entity>
   );
 };
--- client/src/components/VRProfiles.jsx
+++ client/src/components/VRProfiles.jsx
@@ -15,14 +15,6 @@ class VRProfiles extends React.Component {
       friendsToShow: this.props.friends.slice(0, 5)
     };
     this.onMoreFriendsClick = this.onMoreFriendsClick.bind(this);
-    let n = friendsToShow.length;
-    console.log('n',n);
-    let start = (n) * Math.PI/12;
-    console.log('start', start);
-    let theta = (Math.PI - start)/2;
-    console.log('theta', theta);
-    let x, z, yRotation;
-    let radius = 10;
   }
 
   onMoreFriendsClick() {
@@ -40,33 +32,48 @@ class VRProfiles extends React.Component {
   }
 
   render() {
+    let n = this.state.friendsToShow.length;
+    console.log('n',n);
+    let start = (n) * Math.PI/12;
+    console.log('start', start);
+    let theta = (Math.PI - start)/2;
+    console.log('theta', theta);
+    let x, z, yRotation;
+    let radius = 10;
+    let y = -4;
+
     return (
       <Entity>
 
         <Entity
           geometry={{primitive: 'box', width: 2, height: 2, depth: 0.15}}
           material={{color: 'white', opacity: 0.5}}
-          position={{x: -6, y: 0, z: -3.1}}
+          position={{x: -Math.cos(theta)*radius, y: y, z: -Math.sin(theta)*radius}}
+          rotation={{x: -Math.atan(Math.abs(y)/radius)*180/Math.PI, y: ((Math.PI/2) - theta)*180/Math.PI}}
           events={{click: (() => this.onMoreFriendsClick())}}></Entity>
 
         <Entity
           text={{value: 'show\nmore\nfriends', align: 'center', color: 'white', width: 10}}
-          position={{x: -6, y: 0, z: -3}}></Entity>
-
+          position={{x: -Math.cos(theta)*radius, y: y, z: .1 -Math.sin(theta)*radius}}
+          rotation={{x: -Math.atan(Math.abs(y)/radius)*180/Math.PI, y: ((Math.PI/2) - theta)*180/Math.PI}}>
+        </Entity>
         {
           this.state.friendsToShow.map((friend, i) => {
+            theta += (Math.PI/12);
             x = -Math.cos(theta)*radius;
             console.log('x', x);
             z = -Math.sin(theta)*radius;
             console.log('z', z);
+            let xRotation = -Math.atan(Math.abs(y)/radius)*180/Math.PI;
             yRotation = ((Math.PI/2) - theta)*180/Math.PI;
             console.log('yRotation', yRotation);
-            theta += (Math.PI/12);
             return (
               <Profile
                 i={i}
                 x={x}
+                y={y}
                 z={z}
+                xRotation={xRotation}
                 yRotation={yRotation}
                 radius={radius}
                 key={i}
