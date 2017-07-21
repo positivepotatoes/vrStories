56ba702a08a415299b2239aa87cd98921849eda4
David Oh <davidjohis@gmail.com>
2017-07-20 19:14:25 -0700

    fixed double audio bug

--- client/src/components/VRProfile.jsx
+++ client/src/components/VRProfile.jsx
@@ -9,8 +9,9 @@ const VRProfile = props => {
   let picRadius = 1;
   let playing = '';
   let spacing = 0.2;
-  if (props.currentStory.story.id === props.friend.profile.id) {
-    playing = ' I AM PLAYING!!!';
+
+  if (props.currentStory.id === props.friend.profile.id) {
+    playing = ` I AM PLAYING!!! ${props.currentStory.index}`;
     picRadius = 1.2;
   }
 
@@ -23,7 +24,7 @@ const VRProfile = props => {
         animation__rotate={{property: 'rotation', dur: 2000, loop: true, to: '360 360 360'}}
         animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '1.1 1.1 1.1'}}
         position={{x: props.x + spacing, y: 0, z: -3}}
-        events={{click: (() => props.onFriendClick(props.friend, props.i))}}>
+        events={{click: (() => props.onFriendClick(props.friend))}}>
       </Entity>
       <Entity text={{value: props.friend.profile.first + playing, align: 'center', color: 'white', width: 3}} position={{x: props.x, y: -1.3, z: -3}}/>
     </Entity>
