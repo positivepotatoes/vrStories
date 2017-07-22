e1e164fbc73eb7c580e7d45ae11ca5ad1ceeec18
alexanderschnapp <alexanderschnapp@users.noreply.github.com>
2017-07-21 21:52:31 -0700

    Update knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,14 @@
 const config = require('config');
 
-module.exports = config['knex'];
+let knex = {
+  "connection": process.env.PG_URI,
+  "migrations": {
+    "tableName": "knex_migrations",
+    "directory": "db/migrations"
+  },
+  "seeds": {
+    "directory": "db/seeds"
+  }
+};
+
+knex.connection ? module.exports = knex :  module.exports = config['knex'];
