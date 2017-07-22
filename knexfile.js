281ab9d4105636e4766962fc08e781ebedb58b34
Anna Zharkova <annagzh@gmail.com>
2017-07-21 17:15:03 -0700

    heroku

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,26 @@
-const config = require('config');
+// const config = require('config');
+//
+// module.exports = config['knex'];
+const secret = require('./config/secret.js');
 
-module.exports = config['knex'];
+module.exports = {
+  "client": "postgresql",
+  "connection": {
+    "database": secret.PG_DB,
+    "user": secret.PG_USER,
+    "password": secret.PG_PASSWORD,
+    "host": secret.PG_HOST,
+    "port": 5432
+  },
+  "pool": {
+    "min": 1,
+    "max": 2
+  },
+  "migrations": {
+    "tableName": "knex_migrations",
+    "directory": "db/migrations"
+  },
+  "seeds": {
+    "directory": "db/seeds"
+  }
+};
