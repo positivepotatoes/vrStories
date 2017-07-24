16dc07f53a96cfa498573deef0fb47b0c523996b
David Oh <davidjohis@gmail.com>
2017-07-24 00:19:01 -0700

    working progress bar with slight visual changes

--- client/src/components/VRProfile.jsx
+++ client/src/components/VRProfile.jsx
@@ -5,37 +5,39 @@ import { Entity } from 'aframe-react';
 
 const VRProfile = props => {
   let picRadius = 1;
-  let playing = '';
   let spacing = 0.2;
 
 
-  let animateClick, progressBar, progressBarTotal;
+  let animateScaleClick, animateScaleMove, progressBar, progressBarTotal;
   if (props.currentStory.id === props.friend.profile.id) {
-    animateClick = {property: 'scale', dir: 'alternate', dur: 200, easing: 'easeInOutQuad', repeat: 1, to: '1.12 1.12 1.12'};
+    animateScaleClick = {property: 'scale', dir: 'alternate', dur: 200, easing: 'easeInOutQuad', repeat: 1, to: '1.12 1.12 1.12'};
+    // animateScaleMove = {property: 'position', dir: 'to', dur: 200, easing: 'easeInOutQuad', repeat: 1, to: `0 3 0`};
     picRadius = 1.2;
 
     let current = props.currentStoriesDuration.current;
     let max = props.currentStoriesDuration.total;
-    let ratioCompleted = current/max;
+    let ratioCompleted = current / max;
     
-    let progressRadius = 1;
+    let progressRadius = .1;
     let progress = ratioCompleted * picRadius * 2;
     let progressXPos = -picRadius + (picRadius * ratioCompleted);
+    let progressYPos = -picRadius * 1.4;
     
     progressBar = 
       <Entity 
         geometry={{primitive: 'cylinder', radius: progressRadius, height: progress}}
         rotation='0 0 90'
-        material={{color: '#878787', opacity: .8}}
-        position={{y: -2.5, x: progressXPos}}
+        material={{color: '#89b6ff', opacity: .8}}
+        position={{y: progressYPos, x: progressXPos}}
       />;
     progressBarTotal = 
       <Entity 
         geometry={{primitive: 'cylinder', radius: progressRadius, height: picRadius * 2}}
         rotation='0 0 90'
         material={{color: '#b2b2b2', opacity: .3}}
-        position={{y: -2.5}}
+        position={{y: progressYPos}}
       />;
+
   }
 
 
@@ -47,11 +49,15 @@ const VRProfile = props => {
         rotation="0 90 90"
         material={{src: props.friend.profile.img_url}}
         // animation__rotate={{property: 'rotation', dur: 2000, loop: true, to: '360 360 360'}}
-        animation__scale={animateClick}
+        animation__scale={animateScaleClick}
         // animation__yoyo={{property: 'position', dir: 'alternate', dur: 1000, easing: 'easeInSine', loop: true, to: '0 2 0'}}
-        events={{click: (() => props.onFriendClick(props.friend)), mouseenter: props.toggleInEntity, mouseleave: props.toggleInEntity}}>
-      </Entity>
-      <Entity text={{value: props.friend.profile.first + playing, align: 'center', color: 'white', width: 6}} position={{y: -1.8, z: .1}}/>
+        events={{click: (() => props.onFriendClick(props.friend)), mouseenter: props.toggleInEntity, mouseleave: props.toggleInEntity}}
+      />
+      <Entity 
+        text={{value: props.friend.profile.first, align: 'center', color: 'white', width: 6}} 
+        position={{y: - picRadius * 1.2}}
+        animation__move={animateScaleMove}
+      />
       {progressBar}
       {progressBarTotal}
     </Entity>
