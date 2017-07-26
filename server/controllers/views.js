c932074107501e00cb5b958af7decc7f2a92f72c
Anna Zharkova <annagzh@gmail.com>
2017-07-26 11:53:36 -0700

    add writing unique views to db

--- server/controllers/views.js
+++ server/controllers/views.js
@@ -1,8 +1,12 @@
 const models = require('../../db/models');
 
 module.exports.addView = (req, res) => {
-  console.log('req.body in views controller:', req.body);
-  models.View.forge({ profile_id: req.body.profileId, story_id: req.body.storyId })
-    .save();
-  res.end();
+  models.View.where({ profile_id: req.body.profileId, story_id: req.body.storyId }).fetch()
+    .then(view => {
+      if (view === null) {
+        models.View.forge({ profile_id: req.body.profileId, story_id: req.body.storyId })
+          .save();
+      }
+      res.end();
+    });
 };
