1169126de9ff57ebc699ef7a74caee060ab8f039
Alexander Schnapp <alexanderschnapp@gmail.com>
2017-07-29 14:43:11 -0700

    Forget cached files

--- server/middleware/index.js
+++
@@ -1,7 +0,0 @@
-module.exports.auth = require('./auth');
-module.exports.passport = require('./passport');
-module.exports.morgan = require('morgan');
-module.exports.bodyParser = require('body-parser');
-module.exports.flash = require('connect-flash');
-module.exports.cookieParser = require('cookie-parser');
-module.exports.multer = require('multer');
