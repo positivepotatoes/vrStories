2522366728b546822e780c44e702eb76d6eccebf
Alexander Schnapp <alexanderschnapp@gmail.com>
2017-08-02 20:47:06 -0700

    Add animation to views component

--- client/src/components/VRView.jsx
+++ client/src/components/VRView.jsx
@@ -2,7 +2,7 @@ import 'aframe';
 import React from 'react';
 
 const VRView = props => (
-  <a-text value={props.viewer} width="7" align="center" position={`0 ${props.textPos} -10`}></a-text>
+  <a-text value={props.viewer} width="7" align="center" position={`0 ${props.textPos + 1} -9.8`}></a-text>
 );
 
 export default VRView;
--- client/src/components/VRViews.jsx
+++ client/src/components/VRViews.jsx
@@ -11,8 +11,15 @@ class VRViews extends React.Component {
     let textPos = 1.5;
     return (
       <a-entity>
-        <a-text value="Users who viewed your story:" align="center" width="6" position="0 1.5 -10"></a-text>
-        <a-plane width="4" height="4" color="white" opacity="0.5" position="0 0 -10"></a-plane>
+        <a-box 
+          width="4" 
+          height="4" 
+          depth=".3"
+          color="white" 
+          opacity="0.5" 
+          position="0 1 -10">
+        </a-box>
+        <a-text value="Who viewed this story:" align="center" width="6" transparent="false" position="0 2.5 -9.5" color="black"></a-text>
         {this.props.viewers.map(viewer => {
           textPos = textPos - 0.5;
           return ( <VRView viewer={viewer} textPos={textPos}/> );
