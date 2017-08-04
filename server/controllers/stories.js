2929598b300cd4de198b36c1992ba8422b6fd509
Alexander Schnapp <alexanderschnapp@gmail.com>
2017-08-04 10:54:36 -0700

    change bucket

--- server/controllers/stories.js
+++ server/controllers/stories.js
@@ -2,7 +2,7 @@ const models = require('../../db/models');
 const s3 = require('../middleware/s3.js').s3;
 
 module.exports.getStoryByKey = (req, res) => {
-  let params = { Bucket: 'vrstories', Key: req.params.id };
+  let params = { Bucket: 'vrstoriesprod', Key: req.params.id };
   s3.getObject(params, (err, data) => {
     if (err) {
       console.log(err);
